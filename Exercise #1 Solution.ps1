40e42955560970b3d287900af617b26d8006cd3a
BURAK ZAİFOĞLU <100001195+BurakZaif@users.noreply.github.com>
2024-08-27 16:37:02 +0300

    Exercise 1

---
+++ Exercise #1 Solution.ps1
@@ -0,0 +1,22 @@
+﻿$OU = "CN=Users,DC=corporate,DC=com"
+
+$destinationFolder = "C:\Users\bzaifoglu\Desktop"
+$destinationFilePath = Join-Path -Path $destinationFolder -ChildPath "UserDetails.csv"
+
+New-Item -Path $destinationFilePath -ItemType File -Force
+
+try{
+    $userList = Get-ADUser -Filter * -SearchBase $OU -Properties Name, Mail, SamAccountName, Enabled -ErrorAction Stop
+    $userList | Select-Object Name, Mail, SamAccountName, Enabled | Out-File -FilePath $destinationFilePath
+
+}
+catch{
+    Write-Output "OU do not exists" | Out-File -FilePath $destinationFilePath
+}
+
+
+
+
+
+
+
